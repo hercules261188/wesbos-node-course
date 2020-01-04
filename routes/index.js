09a814f3da5c1bbe8f73d938c34a44db0e84db1d
Alex Bespoyasov <forjob159@gmail.com>
2020-01-05 02:23:41 +0500

    Add basic template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 // Do work here
 router.get("/", (req, res) => {
-  res.send("Hey! It works!");
+  res.render("hello", {
+    name: "Alex",
+    dog: "Ceasar"
+  });
 });
 
 router.get("/reverse/:name", (req, res) => {
