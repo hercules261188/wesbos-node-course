d9d28b5c95b9d5fbed74cbdbdba9ed6a0904e2c7
Alex Bespoyasov <forjob159@gmail.com>
2020-01-05 01:16:43 +0500

    Add new route example

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  res.send("Hey! It works!");
+});
+
+router.get("/reverse/:name", (req, res) => {
+  res.send(
+    req.params.name
+      .split("")
+      .reverse()
+      .join("")
+  );
 });
 
 module.exports = router;
