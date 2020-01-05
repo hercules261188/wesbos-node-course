82df1c900fdd52656c1b609c31d36bbcc120f88c
Alex Bespoyasov <forjob159@gmail.com>
2020-01-05 16:17:13 +0500

    Add get and post controllers

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,7 @@ const controllers = require("../controllers/storeController");
 
 router.get("/", controllers.homePage);
 
+router.get("/add", controllers.addStore);
+router.post("/add", controllers.createStore);
+
 module.exports = router;
