956a8997bc54db910d83f5c019c739b2de1a6782
Alex Bespoyasov <forjob159@gmail.com>
2020-01-05 16:40:15 +0500

    Save store model data in DB

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const controllers = require("../controllers/storeController");
+const { catchErrors } = require("../handlers/errorHandlers");
 
 router.get("/", controllers.homePage);
 
 router.get("/add", controllers.addStore);
-router.post("/add", controllers.createStore);
+router.post("/add", catchErrors(controllers.createStore));
 
 module.exports = router;
