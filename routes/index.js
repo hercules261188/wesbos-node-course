7856b1bcd4dbe2501baacf0da61a346c1aeb0fc4
Alex Bespoyasov <forjob159@gmail.com>
2020-01-05 17:45:25 +0500

    Add stores view and store card

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const controllers = require("../controllers/storeController");
 const { catchErrors } = require("../handlers/errorHandlers");
 
-router.get("/", controllers.homePage);
+router.get("/", catchErrors(controllers.getStores));
+router.get("/stores", catchErrors(controllers.getStores));
 
 router.get("/add", controllers.addStore);
 router.post("/add", catchErrors(controllers.createStore));
