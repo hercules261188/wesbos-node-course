489a9af681273ca653d220a961d649c511ef1a73
Alex Bespoyasov <forjob159@gmail.com>
2020-01-08 17:39:02 +0500

    Add upload middleware

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,21 @@ const { catchErrors } = require("../handlers/errorHandlers");
 
 router.get("/", catchErrors(controllers.getStores));
 router.get("/stores", catchErrors(controllers.getStores));
-
 router.get("/add", controllers.addStore);
-router.post("/add", catchErrors(controllers.createStore));
-router.post("/add/:id", catchErrors(controllers.updateStore));
+
+router.post(
+  "/add",
+  controllers.upload,
+  catchErrors(controllers.resize),
+  catchErrors(controllers.createStore)
+);
+
+router.post(
+  "/add/:id",
+  controllers.upload,
+  catchErrors(controllers.resize),
+  catchErrors(controllers.updateStore)
+);
 
 router.get("/stores/:id/edit", catchErrors(controllers.editStore));
 
