c629ce2a5f7cac2ca591f86f39ef08e02451ff6d
Alex Bespoyasov <forjob159@gmail.com>
2020-01-11 02:10:30 +0500

    Add search results limit

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -98,9 +98,11 @@ exports.searchStores = async (req, res) => {
   const stores = await Store.find(
     { $text: { $search: req.query.q } },
     { score: { $meta: "textScore" } } // field projection
-  ).sort({
-    score: { $meta: "textScore" }
-  });
+  )
+    .sort({
+      score: { $meta: "textScore" }
+    })
+    .limit(5);
 
   res.json(stores);
 };
