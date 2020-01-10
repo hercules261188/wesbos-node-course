86fb73586ecd4b5893ecc31d37639c454f17fb8d
Alex Bespoyasov <forjob159@gmail.com>
2020-01-11 02:33:19 +0500

    Add search frontend

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -2,6 +2,9 @@ import "../sass/style.scss";
 
 import { $, $$ } from "./modules/bling";
 import autocomplete from "./modules/autocomplete";
+import typeAhead from "./modules/typeAhead";
 
 // Google requires payment for that, f*** them
 // autocomplete($("#address"), $("#lat"), $("#lng"));
+
+typeAhead($(".search"));
---
+++ public/javascripts/modules/typeAhead.js
@@ -0,0 +1,65 @@
+const axios = require("axios");
+
+function searchResultsHtml(stores) {
+  return stores
+    .map(store => {
+      return `<a href='/store/${store.slug}' class='search__result'>
+      <strong>${store.name}</strong>
+    </a>`;
+    })
+    .join("");
+}
+
+function typeAhead(search) {
+  if (!search) return;
+
+  const input = search.querySelector('input[name="search"]');
+  const results = search.querySelector(".search__results");
+
+  input.on("input", function() {
+    if (!this.value) {
+      results.style.display = "none";
+      return;
+    }
+
+    results.style.display = "block";
+    results.innerHTML = "";
+
+    axios
+      .get(`/api/search?q=${this.value}`)
+      .then(res => {
+        if (!res.data.length) return;
+        results.innerHTML = searchResultsHtml(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  });
+
+  input.on("keyup", e => {
+    if (![38, 40, 13].includes(e.keyCode)) return;
+
+    const activeClass = "search__result--active";
+    const current = search.querySelector(`.${activeClass}`);
+    const items = search.querySelectorAll(".search__result");
+
+    let next;
+    if (e.keyCode === 40 && current) {
+      next = current.nextElementSibling || items[0];
+    } else if (e.keyCode === 40) {
+      next = items[0];
+    } else if (e.keyCode === 38 && current) {
+      next = current.previousElementSibling || items[items.length - 1];
+    } else if (e.keyCode === 38) {
+      next = items[items.length - 1];
+    } else if (e.keyCode === 13 && current.href) {
+      window.location = current.href;
+      return;
+    }
+
+    if (current) current.classList.remove(activeClass);
+    next.classList.add(activeClass);
+  });
+}
+
+export default typeAhead;
