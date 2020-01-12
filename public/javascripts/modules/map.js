7513ead61201094f7a63edab32922f9d8f15d1dd
Alex Bespoyasov <forjob159@gmail.com>
2020-01-12 19:39:34 +0300

    Create basic map with near places

---
+++ public/javascripts/modules/map.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { $ } from "./bling";
+
+const mapOptions = {
+  center: {
+    lat: 43.2,
+    lng: -79.8
+  },
+  zoom: 10
+};
+
+function loadPlaces(map, lat = 43.2, lng = -79.8) {
+  axios.get(`/api/stores/near?lat=${lat}&lng=${lng}`).then(res => {
+    const places = res.data;
+    if (!places.length) return;
+
+    const bounds = new google.maps.LatLngBounds();
+
+    const markers = places.map(place => {
+      const [placeLng, placeLat] = place.location.coordinates;
+      const position = { lat: placeLat, lng: placeLng };
+      bounds.extend(position);
+
+      const marker = new google.maps.Marker({ map, position });
+      marker.place = place;
+      return marker;
+    });
+
+    map.setCenter(bounds.getCenter());
+    map.fitBounds(bounds);
+  });
+}
+
+function makeMap(mapDiv) {
+  if (!mapDiv) return;
+
+  const map = new google.maps.Map(mapDiv, mapOptions);
+  loadPlaces(map);
+
+  const input = $('[name="geolocate"]');
+  const autocomplete = new google.maps.places.Autocomplete(input);
+}
+
+export default makeMap;
