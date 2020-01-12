ece312a4566f57ff6bd2a2bb027930d69cac44ca
Alex Bespoyasov <forjob159@gmail.com>
2020-01-12 20:20:40 +0300

    Add client side like functionality

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -4,9 +4,12 @@ import { $, $$ } from "./modules/bling";
 import autocomplete from "./modules/autocomplete";
 import typeAhead from "./modules/typeAhead";
 import makeMap from "./modules/map";
+import ajaxHeart from "./modules/heart";
 
 // Google requires payment for that, f*** them
 // autocomplete($("#address"), $("#lat"), $("#lng"));
 
 typeAhead($(".search"));
 makeMap($("#map"));
+
+$$("form.heart").on("submit", ajaxHeart);
